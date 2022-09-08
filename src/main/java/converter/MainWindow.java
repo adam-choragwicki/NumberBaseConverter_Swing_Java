d6ceccae269b7a812f48d2896fb271a95e6e1d3a
Adam Choragwicki <adamchor1992@gmail.com>
2022-09-08 14:08:07 +0200

    Fonts adjustment

--- src/main/java/converter/MainWindow.java
+++ src/main/java/converter/MainWindow.java
@@ -12,46 +12,67 @@ public class MainWindow extends JFrame
     {
         super("Number base converter");
         setLocationRelativeTo(null);
-        setMinimumSize(new Dimension(350, 350));
-        getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
+        setMinimumSize(new Dimension(500, 500));
+        getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 
         addComponents();
-
         pack();
+        setResizable(false);
         setVisible(true);
     }
 
     private void addComponents()
     {
-        JLabel labelEnterNumber = new JLabel("Enter number");
+        JLabel labelEnterNumber = new JLabel("Enter number", SwingConstants.CENTER);
+        labelEnterNumber.setFont(Config.labelFont);
+        labelEnterNumber.setAlignmentX(Component.CENTER_ALIGNMENT);
         add(labelEnterNumber);
 
         textFieldEnterNumber = new JTextField();
+        textFieldEnterNumber.setFont(Config.textFieldFont);
+        textFieldEnterNumber.setHorizontalAlignment(JTextField.CENTER);
         addTextFieldEnterNumberTextChangedListener();
         add(textFieldEnterNumber);
 
         JLabel labelFromBase = new JLabel("From base");
+        labelFromBase.setFont(Config.labelFont);
+        labelFromBase.setAlignmentX(Component.CENTER_ALIGNMENT);
         add(labelFromBase);
 
+        DefaultListCellRenderer listRenderer = new DefaultListCellRenderer();
+        listRenderer.setHorizontalAlignment(DefaultListCellRenderer.CENTER);
+
         comboBoxFromBase = new JComboBox<>(Base.generateSupportedBases());
+        comboBoxFromBase.setFont(Config.comboBoxFont);
+        comboBoxFromBase.setRenderer(listRenderer);
         comboBoxFromBase.setSelectedItem(new Base(10));
         add(comboBoxFromBase);
 
         JLabel labelToBase = new JLabel("To base");
+        labelToBase.setFont(Config.labelFont);
+        labelToBase.setAlignmentX(Component.CENTER_ALIGNMENT);
         add(labelToBase);
 
         comboBoxToBase = new JComboBox<>(Base.generateSupportedBases());
+        comboBoxToBase.setFont(Config.comboBoxFont);
+        comboBoxToBase.setRenderer(listRenderer);
         comboBoxToBase.setSelectedItem(new Base(16));
         add(comboBoxToBase);
 
         JButton buttonConvert = new JButton("Convert");
         buttonConvert.addActionListener(e -> processButtonConvertClick());
+        buttonConvert.setAlignmentX(Component.CENTER_ALIGNMENT);
         add(buttonConvert);
 
         JLabel labelResultNumber = new JLabel("Result number");
+        labelResultNumber.setFont(Config.labelFont);
+        labelResultNumber.setAlignmentX(Component.CENTER_ALIGNMENT);
         add(labelResultNumber);
 
         textFieldResult = new JTextField();
+        textFieldResult.setFont(Config.textFieldFont);
+        textFieldResult.setHorizontalAlignment(JTextField.CENTER);
+        textFieldResult.setEditable(false);
         add(textFieldResult);
     }
 
