9d81c5d720bc0caca78323f668236cb16fa5150b
Adam Choragwicki <adamchor1992@gmail.com>
2022-09-08 14:10:35 +0200

    Remove converter text version

--- src/main/java/converter/Main.java
+++ src/main/java/converter/Main.java
@@ -1,46 +1,9 @@
 package converter;
 
-import java.util.Scanner;
-
 public class Main
 {
     public static void main(String[] args)
     {
-        runConverterGuiVersion();
-    }
-
-    private static void runConverterGuiVersion()
-    {
         new MainWindow();
-    }
-
-    private static void runConverterTextVersion()
-    {
-        Scanner scanner = new Scanner(System.in);
-
-        while (true)
-        {
-            System.out.print("Enter number to be converted in format: {source base} {target base} {number} ");
-
-            String sourceBaseString = scanner.next();
-            String targetBaseString = scanner.next();
-            String numberString = scanner.next();
-
-            try
-            {
-                Converter converter = new Converter(sourceBaseString, targetBaseString);
-
-                String result = converter.convertNumber(numberString);
-                System.out.print("Conversion result: " + result);
-            }
-            catch (InvalidNumberBaseException | InvalidNumberRepresentationException ignored)
-            {
-
-            }
-            finally
-            {
-                System.out.print("\n\n");
-            }
-        }
     }
 }
